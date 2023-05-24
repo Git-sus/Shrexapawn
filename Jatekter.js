e48fadcc2bb0d2c7ceab8f686843f0e850088c3c
malackapite <veress.marcell@protonmail.com>
2023-05-24 11:21:10 +0200

    jatekter :cat2::dash:

--- Jatekter.js
+++ Jatekter.js
@@ -1,22 +1,55 @@
 import { Mezo } from "/Mezo.js";
 
 export class Jatekter{
-    #a=null
+    #mezoLista=[]
+    #kor
+    #kattintottMezo=null
     constructor(){    
+        this.#kor=1
         for (let ix = 0; ix < 9; ix++) {
-            new Mezo("main",ix)        
+            this.#mezoLista.push(new Mezo("main",ix))  
         }
-        
+        this.jatekosLep()
+    }
+
+    mezoListaToString() {
+        let tmp=""
+        this.#mezoLista.forEach(element => {
+            tmp+=element.babu
+        })
+        return tmp
+    }
+
+    jatekosLep(){
         $(window).on("elemValaszt",event =>{
-            if(this.#a===null)
-                this.#a=event.detail
-            else{
-                if(this.#a.LegalisLepes(event.detail) || this.#a.LegalisTamadas(event.detail)){
-                    this.#a.csere(event.detail)
+            console.log(this.mezoListaToString());
+            if(this.#kor%2){
+                if(ellenorzes()){
+                    if(this.#kattintottMezo===null)
+                        this.#kattintottMezo=event.detail
+                    else{
+                        if(this.#kattintottMezo.LegalisLepes(event.detail) || this.#kattintottMezo.LegalisTamadas(event.detail)){
+                            this.#kattintottMezo.csere(event.detail)
+                            this.#kor++
+                            this.gepLep()
+                        }
+                        this.#kattintottMezo=null
+                    }
                 }
-                this.#a=null
+                else{
+                    console.log("vesztettél");
+                }
             }
         })  
-        
+    }
+    gepLep(){
+        setTimeout(() => {
+            console.log("geplep");
+            this.#kor++
+        }, 2000);
+    }
+
+    ellenorzes(){
+        //tdod
     }
 }
